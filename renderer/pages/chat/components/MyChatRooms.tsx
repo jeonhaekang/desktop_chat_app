69643ad9ad593b5b45d6995a75e993617afecd12
kkangs <1003gorkd@naver.com>
2023-01-21 01:34:21 +0900

    feat : 참가인원 닉네임 표기

--- renderer/pages/chat/components/MyChatRooms.tsx
+++ renderer/pages/chat/components/MyChatRooms.tsx
@@ -1,19 +1,19 @@
 import styles from '@/styles/pages/chat/components/rooms.module.scss';
 
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import { useMount, useRouter } from '@/utils/hooks';
+import { filterKeyOfArr } from '@/utils/common';
 
 import { userAuth, userRoomsDB } from '@/firebase/models';
 import { DataSnapshot } from 'firebase/database';
 
 import { IUserRooms } from '@/types/chat';
+import { IUser } from '@/types/account';
 
 const MyChatRooms = () => {
   const { push } = useRouter();
 
   const [rooms, setRooms] = useState<IUserRooms[]>([]);
-
-  console.log(rooms);
 
   useMount(async () => {
     try {
@@ -34,11 +34,14 @@ const MyChatRooms = () => {
   return (
     <ul className={styles.rooms}>
       {rooms.map((room) => {
-        const { roomId, lastMessage } = room;
+        const { roomId, lastMessage, users } = room;
+
+        const displayNames = filterKeyOfArr<IUser>(users, 'displayName');
 
         return (
           <li key={roomId} className={styles.room} onDoubleClick={() => push(`/chat/${roomId}`)}>
-            {lastMessage}
+            <p className={styles.users}>{displayNames.join(' , ')}</p>
+            <p className={styles.lastMessage}>{lastMessage}</p>
           </li>
         );
       })}
