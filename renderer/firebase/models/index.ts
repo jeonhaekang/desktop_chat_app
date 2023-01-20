95e839837915fb28474b955e12242ad48d08dfcf
kkangs <1003gorkd@naver.com>
2023-01-21 03:06:46 +0900

    fix : 잘못된 임포트 삭제

--- renderer/firebase/models/index.ts
+++ renderer/firebase/models/index.ts
@@ -1,8 +1,7 @@
-import { alertError } from './Base';
 import userAuth from './UserAuth';
 import usersDB from './UsersDB';
 import roomsDB from './RoomsDB';
 import messagesDB from './MessagesDB';
 import userRoomsDB from './UserRoomsDB';
 
-export { userAuth, usersDB, roomsDB, messagesDB, userRoomsDB, alertError };
+export { userAuth, usersDB, roomsDB, messagesDB, userRoomsDB };
