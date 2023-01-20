48d9e1d8a04658721445a8c0bb601fd193cab5e6
kkangs <1003gorkd@naver.com>
2023-01-20 11:34:01 +0900

    feat : button 컴포넌트 생성

---
+++ renderer/components/button/Button.tsx
@@ -0,0 +1,21 @@
+import styles from './button.module.scss';
+import cn from 'classnames';
+
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+
+interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color?: 'blue' | 'white' | 'red';
+  size?: 'medium' | 'small';
+  full?: boolean;
+  children: ReactNode;
+}
+
+const Button = ({ color = 'blue', size = 'medium', children, ...rest }: IProps) => {
+  return (
+    <button className={cn(styles.button, styles[color], styles[size])} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
