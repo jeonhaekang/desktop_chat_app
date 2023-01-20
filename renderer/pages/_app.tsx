8ca8a82251d51f93d43334fc630e157d43927482
kkangs <1003gorkd@naver.com>
2023-01-20 19:58:22 +0900

    feat : rootContainer 생성 및 기타 style 수정

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,7 +1,12 @@
+import styles from '@/styles/pages/app.module.scss';
 import '@/styles/index.scss';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.rootContainer}>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default App;
