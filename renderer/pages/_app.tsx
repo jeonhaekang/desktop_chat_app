d12b7d5d897b1e408837582ccbb84b347ffac9f0
kkangs <1003gorkd@naver.com>
2023-01-20 11:16:22 +0900

    initialize

---
+++ renderer/pages/_app.tsx
@@ -0,0 +1,7 @@
+import "../styles/index";
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ renderer/pages/home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>메인페이지</title>
+      </Head>
+
+      <div>메인페이지</div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
