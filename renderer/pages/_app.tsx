1619b23bd91c63996fa6488200e7a585813ba3f1
kkangs <1003gorkd@naver.com>
2023-01-20 14:30:27 +0900

    feat : rootContainer 지정

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,7 +1,12 @@
 import '@/styles/index.scss';
+import styles from '@/styles/pages/app.module.scss';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.rootContainer}>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default App;
