c4fa78e72ad44e7bdf841b7d8c4a4b5e2c568b26
kkangs <1003gorkd@naver.com>
2023-01-22 12:14:45 +0900

    feat : sample firebase config

---
+++ renderer/firebase/config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const config = {
+  apiKey: 'YOUR API KEY',
+  authDomain: 'YOUR AUTH DOMAIN',
+  databaseURL: 'YOUR DATABASE URL',
+  projectId: 'YOUR PROJECT ID',
+  storageBucket: 'USER STORAGE BUCKET',
+  messagingSenderId: 'YOUR MESSAGING SENDER ID',
+  appId: 'YOUR APP ID',
+};
+
+const app = initializeApp(config);
+
+export default app;
